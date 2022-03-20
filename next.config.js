c0b45826f3dd931a78a86431cffb518ed6907990
minhong <z574512z@gmail.com>
2022-03-20 19:56:59 +0800

    chore: updated static export and install tailwind css

--- next.config.js
+++ next.config.js
@@ -1,6 +1,24 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  exportPathMap: async function (
+    defaultPathMap,
+    { dev, dir, outDir, distDir, buildId }
+  ) {
+    return {
+      '/': { page: '/' },
+    }
+  },
+  assetPrefix: "./",
+  images: {
+    loader: "akamai",
+    path: ""
+  }
 }
 
 module.exports = nextConfig
+
+
+
+
+
