4e2e688df94d9474311598cfcab3b78452782797
minhong <z574512z@gmail.com>
2022-03-23 01:09:55 +0800

    feat: updated font

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,6 +8,9 @@ class MyDocument extends Document {
           <meta property="description" content="justfont 相信字體的價值，更相信好的字體能啟發社會。除了細心栽培一筆一劃，我們更願做字體推廣教育的先行者、教育者與輔導者，致力於培育台灣下一代的設計力量。從基礎建設與教育開始，提升台灣的美學環境。邀請您一起來，創造更好的文字風景。"/>
           <link rel="preconnect" href="https://fonts.googleapis.com"/>
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true"/>
+          <link href="https://fonts.googleapis.com/css2?family=Baskervville&display=swap" rel="stylesheet"/>
+          <link rel="preconnect" href="https://fonts.googleapis.com"/>
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true"/>
           <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet"/>
         </Head>
         <body>
