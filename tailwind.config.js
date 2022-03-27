8a7c84e768b9a7869e0e3fdd25548e6883b8b46e
Min <min@3drens.com>
2022-03-28 02:10:29 +0800

    feat: create arrow animate

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,12 +7,23 @@ module.exports = {
     extend: {
       keyframes: {
         scroll: {
-          '0%': { transform: ' translateX(0)' },
+          '0%': { transform: 'translateX(0)' },
           '100%': { transform: 'translateX(-2400px)' },
+        },
+        arrow: {
+          '0%, 100%': {
+            transform: 'translateY(50%)',
+            'animation-timing-function': 'cubic - bezier(0.8, 0, 1, 1)',
+          },
+          '50%': {
+            transform: 'translateY(0)',
+            'animation-timing-function': 'cubic - bezier(0, 0, 0.2, 1)',
+          },
         },
       },
       animation: {
         scroll: 'scroll 26s linear infinite',
+        arrow: 'arrow 3s ease-in-out infinite',
       },
     },
   },
