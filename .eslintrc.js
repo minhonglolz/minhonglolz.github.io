c4d047d9eff7264f2ba68429ae1ff5e3bf9d0f44
Min <min@3drens.com>
2022-04-03 17:36:43 +0800

    chore: update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -36,5 +36,56 @@ module.exports = {
     'react/self-closing-comp': 'warn',
     'react/jsx-fragments': 'warn',
     'react/jsx-equals-spacing': [2, 'never'],
+    /** https://typescript-eslint.io/rules/comma-dangle */
+    '@typescript-eslint/comma-dangle': ['error', {
+      arrays: 'always-multiline',
+      objects: 'always-multiline',
+      imports: 'always-multiline',
+      exports: 'always-multiline',
+      functions: 'only-multiline',
+      generics: 'ignore',
+    }],
+    /** https://typescript-eslint.io/rules/indent/ */
+    indent: 'off',
+    '@typescript-eslint/indent': ['error', 2, {
+      SwitchCase: 1,
+      VariableDeclarator: 1,
+      outerIIFEBody: 1,
+      MemberExpression: 1,
+      FunctionDeclaration: { parameters: 1, body: 1 },
+      FunctionExpression: { parameters: 1, body: 1 },
+      CallExpression: { arguments: 1 },
+      ArrayExpression: 1,
+      ObjectExpression: 1,
+      ImportDeclaration: 1,
+      flatTernaryExpressions: false,
+      ignoreComments: false,
+      ignoredNodes: ['TemplateLiteral *', 'JSXElement', 'JSXElement > *', 'JSXAttribute', 'JSXIdentifier', 'JSXNamespacedName', 'JSXMemberExpression', 'JSXSpreadAttribute', 'JSXExpressionContainer', 'JSXOpeningElement', 'JSXClosingElement', 'JSXFragment', 'JSXOpeningFragment', 'JSXClosingFragment', 'JSXText', 'JSXEmptyExpression', 'JSXSpreadChild'],
+      offsetTernaryExpressions: true,
+    }],
+    /** https://typescript-eslint.io/rules/type-annotation-spacing/ */
+    '@typescript-eslint/type-annotation-spacing': ['error'],
+    /** https://typescript-eslint.io/rules/member-delimiter-style */
+    '@typescript-eslint/member-delimiter-style': ['error', {
+      multiline: {
+        delimiter: 'comma',
+        requireLast: true,
+      },
+      singleline: {
+        delimiter: 'comma',
+        requireLast: false,
+      },
+      multilineDetection: 'brackets',
+    }],
+    /** https://typescript-eslint.io/rules/no-unused-vars */
+    /** https://typescript-eslint.io/rules/space-before-function-paren */
+    'space-before-function-paren': 'off',
+    '@typescript-eslint/space-before-function-paren': ['error', 'always'],
+    /** https://typescript-eslint.io/rules/space-infix-ops */
+    'space-infix-ops': 'off',
+    '@typescript-eslint/space-infix-ops': ['error'],
+    /** https://typescript-eslint.io/rules/semi */
+    semi: 'off',
+    '@typescript-eslint/semi': ['error', 'never'],
   },
 }
