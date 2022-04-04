87977ce7f0174f0c3097b31e9876c945521f2f9c
Min <min@3drens.com>
2022-04-04 22:01:47 +0800

    feat: add homeBackground animate

--- components/HomeBackground.tsx
+++ components/HomeBackground.tsx
@@ -1,15 +1,19 @@
 
 import { HTMLAttributes } from 'react'
 import background from '../assets/images/cleanersBG.jpg'
+import animationDelayStyle from '../libs/animationDelayStyle'
 
 export default function HomeBackground (props: HTMLAttributes<'div'>) {
   return (
-    <div
-      id={props.id}
-      className="text-white h-screen w-full bg-cover bg-center flex pt-5"
-      style={{ backgroundImage: `url(${background.src})` }}
-    >
-      <div/>
+    <div className="bg-black">
+      <div
+        id={props.id}
+        className="text-white h-screen w-full bg-cover bg-center flex pt-5 opacity-0 animate-opacity0To1"
+        style={{ backgroundImage: `url(${background.src})`, ...animationDelayStyle(3.3) }}
+      >
+        <div/>
+      </div>
     </div>
+
   )
 };
