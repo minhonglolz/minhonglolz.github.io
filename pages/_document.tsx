fa240da4c853a972c0ad3c8b5b1f5f6cf3b15dc9
Min <min@3drens.com>
2023-07-30 15:56:39 +0800

    feat: add web title

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-title-in-document-head */
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 
 class MyDocument extends Document {
@@ -5,7 +6,8 @@ class MyDocument extends Document {
     return (
       <Html className="scroll-smooth">
         <Head>
-          <meta property="description" content="Min、黃閔弘、Minhonglolz"/>
+          <title>minhong</title>
+          <meta property="og:title" key="title" content="Min、黃閔弘、Minhonglolz、z574512z@gmail.com、front-end、前端工程師"/>
           <link rel="preconnect" href="https://fonts.googleapis.com"/>
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true"/>
           <link href="https://fonts.googleapis.com/css2?family=Mulish:wght@400;600&display=swap" rel="stylesheet"/>
